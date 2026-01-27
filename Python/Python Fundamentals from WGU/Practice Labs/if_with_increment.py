deb45176c1edf013d49dc32e93dba54f68d7f83a
David Mitchell <dmaple301@gmail.com>
2026-01-27 13:13:56 -0500

    Delete Python directory

--- Python/Python Fundamentals from WGU/Practice Labs/if_with_increment.py
+++
@@ -1,7 +0,0 @@
-values = [2,4,6,8]
-sum = 0
-for number in values:
-    if sum < 20:
-        sum = sum + number
-print(sum)
-
--- Python/ping3 imported test.py
+++
@@ -1,7 +0,0 @@
-from ping3 import ping
-
-response_time = ping("8.8.8.8")
-if response_time is not None:
-    print(f"Ping successful: {response_time:.2f} seconds")
-else:
-    print("Ping failed")
